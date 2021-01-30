e671fd5345e8c740fa8dfff656ca1c06cbdfb279
mdo91 <hesoka_mxx@yahoo.com>
2021-01-30 10:05:15 +0300

    Initial Commit

---
+++ stats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stats
+//
+//  Created by Mdo on 30/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
